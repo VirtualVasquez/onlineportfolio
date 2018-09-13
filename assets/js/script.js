31ae9e4ef2517a1c3df3402a596a38aebdcc5e32
VirtualVasquez <mxv647@med.miami.edu>
2018-09-12 20:37:21 -0400

    reworking to make single page, still mapping contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+
+//Project portfolio names
 //sample urls and names only
 var repository = {
 	"projects": [
@@ -16,13 +18,33 @@ var repository = {
 		{
 			"name": "testing",
 			"url": "https://virtualvasquez.github.io/Hangman-Game/"
-		}
+		},
+		{
+			"name": "testing",
+			"url": "https://virtualvasquez.github.io/Hangman-Game/"
+		},
+		{
+			"name": "testing",
+			"url": "https://virtualvasquez.github.io/Hangman-Game/"
+		},
+		{
+			"name": "testing",
+			"url": "https://virtualvasquez.github.io/Hangman-Game/"
+		},
+		{
+			"name": "testing",
+			"url": "https://virtualvasquez.github.io/Hangman-Game/"
+		},
+		{
+			"name": "testing",
+			"url": "https://virtualvasquez.github.io/Hangman-Game/"
+		}							
 	]
 }
 
 $(document).ready(function(){
 	var projectTemplate = $("#project-template").html();
 	var compiledProjectTemplate = Handlebars.compile(projectTemplate);
-	$("#NOTHING").append(compiledProjectTemplate(repository));
+	$("#target").append(compiledProjectTemplate(repository));
 
 });
