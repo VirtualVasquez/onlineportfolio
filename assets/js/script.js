02beea3ae4d0c95435978f69f5bf983971c5d55b
VirtualVasquez <mxv647@med.miami.edu>
2018-09-15 17:17:46 -0400

    handlebars for jobs and projects work simultaneously

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
-
-//Project portfolio names
-//sample urls and names only
-var repository = {
+const repository = {
 	"projects": [
 		{
 			"name": "Undertale HangMan",
-			"url": "https://virtualvasquez.github.io/Hangman-Game/"
+			"url": "https://virtualvasquez.github.io/Hangman-Game/",
 		},
 		{
 			"name": "burgers",
@@ -42,9 +39,26 @@ var repository = {
 	]
 }
 
+
+const history = {
+	"jobs": [
+		{
+			"title": "Bilingual Flooring Retail Associate",
+			"shortcode": "thd",
+			"start": "June 2014",
+			"end": "December 2016",
+			"descript": "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type"
+		}
+
+	]
+}
+
 $(document).ready(function(){
 	var projectTemplate = $("#project-template").html();
 	var compiledProjectTemplate = Handlebars.compile(projectTemplate);
-	$("#target").append(compiledProjectTemplate(repository));
+	$("#targetOne").append(compiledProjectTemplate(repository));
 
+	var jobTemplate = $("#job-template").html();
+	var compiledJobTemplate = Handlebars.compile(jobTemplate);
+	$("#targetTwo").html(compiledJobTemplate(history));
 });
