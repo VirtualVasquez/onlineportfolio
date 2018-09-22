bfe701c305ed1f5469b71c2b64d127d7fb2f1191
VirtualVasquez <mxv647@med.miami.edu>
2018-09-22 16:22:27 -0400

    images added to portfolio portion

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ const repository = {
 			"url": "https://virtualvasquez.github.io/Hangman-Game/",
 			"repo": "https://github.com/VirtualVasquez/Hangman-Game",
 			"preview": "Hangman, made with javascript and determination.",
-			"picture": "sans"
+			"picture": "heart"
 		},
 		{
 			"name": "Emerald Escalation",
@@ -19,14 +19,14 @@ const repository = {
 			"url": "https://virtualvasquez.github.io/TriviaGame/",
 			"repo": "https://github.com/VirtualVasquez/TriviaGame",
 			"preview": "Test your knowledge of Mega Man X against the clock.",
-			"picture": ""
+			"picture": "mmx"
 		},
 		{
 			"name": "Animating Anime",
 			"url": "https://virtualvasquez.github.io/Ajax-Gifs/",
 			"repo": "https://github.com/VirtualVasquez/Ajax-Gifs",
 			"preview": "Using APIs to get you G-I-F's of A-ni-me.",
-			"picture":
+			"picture": "anime"
 		},
 		{
 			// NOT A WEBSITE
@@ -34,7 +34,7 @@ const repository = {
 			// NO URL AVAILABLE -- FIX REPO WITH SCREENSHOTS!
 			"repo": "https://github.com/VirtualVasquez/week-4-game",
 			"preview": "Hey LIRI, demonstrate APIs and Node.js in the terminal.",
-			"picture":
+			"picture": "liri"
 		},
 		{
 			//NOT A WEBSITE
@@ -42,28 +42,28 @@ const repository = {
 			// NO URL AVIALABLE
 			"repo": "https://github.com/VirtualVasquez/Node-MySQL-Storefront",
 			"preview": "An introduction to MySQL Databases in the terminal.",
-			"picture":
+			"picture": "cash"
 		},
 		{
 			"name": "Friend Finder",
 			"url": "https://obscure-fjord-17821.herokuapp.com/",
 			"repo": "https://github.com/VirtualVasquez/FriendFinder",
 			"preview": "An Express delivery of a new friend.",
-			"picture":
+			"picture": "radar"
 		},
 		{
 			"name": "Burgers",
 			"url": "https://sleepy-fortress-91819.herokuapp.com/",
 			"repo": "https://github.com/VirtualVasquez/burger",
 			"preview": "Getting a handle on your burgers, and serving them to and from a database.",
-			"picture":
+			"picture": "burger"
 		},	
 		{
 			"name": "Almanac JS",
 			"url": "https://almanac-js.herokuapp.com/",
 			"repo": "https://github.com/VirtualVasquez/Team-Fourtress",
 			"preview": "A group effort to learn new javascript methods on a whim.",
-			"picture":
+			"picture": "book"
 		}	
 	]
 }
