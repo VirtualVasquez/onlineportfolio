a3aaf19ce80348988b4301158a1b6be58ef453e9
VirtualVasquez <mxv647@med.miami.edu>
2018-09-26 15:11:08 -0400

    removing work experience portion

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,39 +67,39 @@ const repository = {
 }
 
 
-const history = {
-	"jobs": [
-		{
-			"title": "Bilingual Flooring Retail Associate",
-			"shortcode": "thd",
-			"start": "June 2014",
-			"end": "December 2016",
-			"descript": "Working in cooperation with members both in and outside of my department, my overall goal was to greet, qualify, recommmend, and close every customer. In addition, maintenance of our deparment, both virtually through information software applications, and physically, via upkeep of decorum using heavy machinery to restock and retrieve merchandise throughout the warehouse."
-		},
-		{
-			"title": "Chiropractic Assistant",
-			"shortcode": "usa",
-			"start": "December 2016",
-			"end": "April 2017",
-			"descript": "Responsible for scheduling patients for their appointments, collecting patient information, keeping track of and charging for patient's payments, guiding patients through therapuetic exercises and stretches."
-		},
-		{"title": "Clinical Assistant - Inpatient Physical Therapy",
-			"shortcode": "um",
-			"start": "May 2017",
-			"end": "Present (September 2018)",
-			"descript": ""
-		}
+// const history = {
+// 	"jobs": [
+// 		{
+// 			"title": "Bilingual Flooring Retail Associate",
+// 			"shortcode": "thd",
+// 			"start": "June 2014",
+// 			"end": "December 2016",
+// 			"descript": "Working in cooperation with members both in and outside of my department, my overall goal was to greet, qualify, recommmend, and close every customer. In addition, maintenance of our deparment, both virtually through information software applications, and physically, via upkeep of decorum using heavy machinery to restock and retrieve merchandise throughout the warehouse."
+// 		},
+// 		{
+// 			"title": "Chiropractic Assistant",
+// 			"shortcode": "usa",
+// 			"start": "December 2016",
+// 			"end": "April 2017",
+// 			"descript": "Responsible for scheduling patients for their appointments, collecting patient information, keeping track of and charging for patient's payments, guiding patients through therapuetic exercises and stretches."
+// 		},
+// 		{"title": "Clinical Assistant - Inpatient Physical Therapy",
+// 			"shortcode": "um",
+// 			"start": "May 2017",
+// 			"end": "Present (September 2018)",
+// 			"descript": ""
+// 		}
 
-	]
-}
+// 	]
+// }
 
 $(document).ready(function(){
 	var projectTemplate = $("#project-template").html();
 	var compiledProjectTemplate = Handlebars.compile(projectTemplate);
 	$("#targetOne").html(compiledProjectTemplate(repository));
 
-	var jobTemplate = $("#job-template").html();
-	var compiledJobTemplate = Handlebars.compile(jobTemplate);
-	$("#targetTwo").html(compiledJobTemplate(history));
+	// var jobTemplate = $("#job-template").html();
+	// var compiledJobTemplate = Handlebars.compile(jobTemplate);
+	// $("#targetTwo").html(compiledJobTemplate(history));
 });
 
